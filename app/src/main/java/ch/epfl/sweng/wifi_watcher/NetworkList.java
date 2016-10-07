0343684e68fe2cb38b369498a820950b282d3082
stdgregwar <gregoirehirt@gmail.com>
2016-10-07 18:14:12 +0200

    app base

---
+++ app/src/main/java/ch/epfl/sweng/wifi_watcher/NetworkList.java
@@ -0,0 +1,13 @@
+package ch.epfl.sweng.wifi_watcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NetworkList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_network_list);
+    }
+}
